a4411708af4654dbe49adc11eb9c3df667404565
thenielthevis <thenielthevis>
2024-07-09 20:25:31 +0800

    favicon

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'KCLYNE')</title>
-    
+    <link rel="icon" type="image/x-icon" href="/Images/Layouts/favicon.ico">    
     <link href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,200&display=swap" rel="stylesheet">
 
